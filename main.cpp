36c7f382b702c0886d6fc291339f7d3c75cfd77a
Qianyu Jiang <114326441+jiangqianyu@users.noreply.github.com>
2024-03-12 16:33:51 +0800

    Add files via upload
    ### 代码概括
    泊位结构体添加了是否有船、货物队列；
    船只结构体添加了物品数量、物品价值，具体状态（区分为LOAD,DONE,TO_BERTH,TO_VIRTUAL）
    在每一帧的input时更新船只的具体状态，并装载货物。
    在虚拟点时思考命令：(`GetBerthPriority` 函数)基于货物总价值、运输时间、是否被占用和装载速度计算。
    在装载时思考命令： (`BoatReadyGo` 函数)判断船只是否装满或装载的货物价值达到阈值。
    
    ### 未完成的部分
    本地测试，官网测试没语法错误，但没考虑语义有没有bug。。。。
    优化GetBerthPriority函数，GetThreshold函数
    在TO_VIRTUAL路上还可以回泊位多装点
    在TO_BERTH路上还可以改变目标泊位
    wait时可以更换目标泊位

--- main.cpp
+++ main.cpp
@@ -25,22 +25,43 @@ struct Berth
     int y;
     int transport_time;
     int loading_speed;
-    Berth() {}
+    bool is_occupied; // 如果有船占用或有船驶向此泊位，则为true
+    queue<int> goods; // 待装载的货物
+    Berth()
+    {
+        is_occupied = false;
+    }
     Berth(int x, int y, int transport_time, int loading_speed)
     {
         this->x = x;
         this->y = y;
         this->transport_time = transport_time;
         this->loading_speed = loading_speed;
+        is_occupied = false;
     }
 } berth[berth_num + 10];
+
+// 船的specific_status
+#define WAIT 3
+#define LOAD 4
+#define DONE 5
+#define TO_BERTH 6
+#define TO_VIRTUAL 7
 
 struct Boat
 {
     // 船的容积是一样的
-    int num;    // 携带物品的价值
-    int pos;    // 目标泊位，-1表示虚拟点
-    int status; // 0表示移动（从地图上消失），1表示装货状态或运输完成，2表示等待
+    int num;             // 船上物品的数量
+    int goods_value;     // 船上物品的价值
+    int pos;             // 目标泊位，-1表示虚拟点
+    int status;          // 0表示移动（从地图上消失），1表示装货状态或运输完成，2表示等待
+    int specific_status; // WAIT表示等待，LOAD表示装货，DONE表示运输完成，TO_BERTH表示去泊位，TO_VIRTUAL表示去虚拟点
+    Boat()
+    {
+        num = 0;
+        goods_value = 0;
+        specific_status = DONE;
+    }
 } boat[10];
 
 int money, boat_capacity, id;
@@ -62,6 +83,40 @@ void Init()
     printf("OK\n");
     fflush(stdout);
 }
+/**
+ * \brief 维护船只的具体状态，
+ * WAIT表示等待，LOAD表示装货，DONE表示运输完成，TO_BERTH表示去泊位，TO_VIRTUAL表示去虚拟点；
+ * 以及船只目前装载的货物的数量和价值，泊位的货物队列
+ */
+void UpdateBoatSpecificState(int boat_id)
+{
+    if (boat[boat_id].status == 1)
+    {
+        if (boat[boat_id].specific_status == TO_BERTH || boat[boat_id].specific_status == WAIT)
+        {
+            boat[boat_id].specific_status = LOAD;
+        }
+        else if (boat[boat_id].specific_status == TO_VIRTUAL)
+        {
+            boat[boat_id].specific_status = DONE;
+        }
+    }
+    else if (boat[boat_id].status == 2)
+    {
+        boat[boat_id].specific_status = WAIT;
+    }
+    if (boat[boat_id].specific_status == LOAD)
+    {
+        for (int i = 0; i < berth[boat[boat_id].pos].loading_speed; i++)
+        {
+            if (berth[boat[boat_id].pos].goods.empty())
+                break;
+            boat[boat_id].goods_value += berth[boat[boat_id].pos].goods.front();
+            berth[boat[boat_id].pos].goods.pop();
+            boat[boat_id].num += 1;
+        }
+    }
+}
 
 int Input()
 {
@@ -78,14 +133,136 @@ int Input()
     {
         int sts;
         scanf("%d%d%d%d", &robot[i].goods, &robot[i].x, &robot[i].y, &sts);
-        // 根据状态处理机器人........
     }
     for (int i = 0; i < 5; i++)
         scanf("%d%d\n", &boat[i].status, &boat[i].pos);
-    // 处理船只........
+    // 维护船只的具体状态........
+    for (int i = 0; i < 5; i++)
+        UpdateBoatSpecificState(i);
     char okk[100];
     scanf("%s", okk);
     return id;
+}
+
+/**
+ * \brief 计算目前泊位上船只可装下的货物的总价值
+ */
+int GetTotalValue(int berth_id)
+{
+    int total_value = 0; // 能装载的货物的总价值
+    int cnt = 0;         // 目前装载的货物的数量
+    while (cnt < boat_capacity && cnt < berth[berth_id].goods.size())
+    {
+        if (berth[berth_id].goods.empty())
+            break;
+        int tmp = berth[berth_id].goods.front();
+        total_value += tmp;
+        berth[berth_id].goods.pop();
+        berth[berth_id].goods.push(tmp);
+        cnt++;
+    }
+    return total_value;
+}
+
+// 超参数
+int total_value_weight = 100;
+int transport_time_weight = 10;
+int is_occupied_weight = 1000;
+int loading_speed_weight = 1;
+
+/**
+ * \brief 需要考虑如何计算泊位的优先级......
+ */
+int GetBerthPriority(int berth_id)
+{
+    int priority = 0;
+    int total_value = GetTotalValue(berth_id);
+    // 计算泊位的优先级
+    if (berth[berth_id].is_occupied)
+    {
+        priority = total_value * total_value_weight - berth[berth_id].transport_time * transport_time_weight - is_occupied_weight - berth[berth_id].loading_speed * loading_speed_weight;
+    }
+    else
+    {
+        priority = total_value * total_value_weight - berth[berth_id].transport_time * transport_time_weight - berth[berth_id].loading_speed * loading_speed_weight;
+    }
+    return priority;
+}
+
+/**
+ * \brief 获取船只的目标泊位
+ */
+int GetBerthId()
+{
+    int priority[berth_num + 10]; // 计算泊位的优先级，数值越大优先级越高
+    int tmp_priority;
+    for (int i = 0; i < berth_num; i++)
+    {
+        tmp_priority = GetBerthPriority(i);
+        priority[i] = tmp_priority;
+    }
+    // 选择优先级最高的泊位
+    int max_priority = -99999;
+    int max_priority_id = -1;
+    for (int i = 0; i < berth_num; i++)
+    {
+        if (priority[i] > max_priority)
+        {
+            max_priority = priority[i];
+            max_priority_id = i;
+        }
+    }
+    berth[max_priority_id].is_occupied = true;
+    return max_priority_id;
+}
+
+/**
+ * \brief 装了多少货物后，船只离开泊位进行运输，需要考虑怎么设置阈值.......
+ */
+int GetThreshold(int boat_id)
+{
+    int threshold = berth[boat[boat_id].pos].transport_time;
+    return threshold;
+}
+
+bool BoatReadyGo(int boat_id)
+{
+    int threshold = GetThreshold(boat_id);
+    // 船只满载或者价值超过阈值
+    if (boat[boat_id].num >= boat_capacity || boat[boat_id].goods_value >= threshold)
+    {
+        return true;
+    }
+    else
+    {
+        return false;
+    }
+}
+
+/**
+ * \brief 只考虑了在虚拟点时需下达命令，在泊位装货时需下达命令.......
+ */
+void GiveBoatCommand()
+{
+    for (int i = 0; i < 5; i++)
+    {
+        if (boat[i].specific_status == DONE)
+        { // 如果船只运输完成，在虚拟点
+            boat[i].specific_status = TO_BERTH;
+            printf("ship %d %d\n", i, GetBerthId());
+            continue;
+        }
+        if (boat[i].specific_status == LOAD)
+        {
+            // 如果船只在泊位装货
+            if (BoatReadyGo(i))
+            {
+                boat[i].specific_status = TO_VIRTUAL;
+                printf("go %d\n", i);
+            }
+            continue;
+        }
+    }
 }
 
 int main()
@@ -98,9 +275,9 @@ int main()
             // 为机器人下达命令........
             printf("move %d %d\n", i, rand() % 4);
         // 为船只下达命令........
+        void GiveBoatCommand();
         puts("OK");
         fflush(stdout);
     }
-
     return 0;
 }
